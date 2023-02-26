17a9394749635d6a2bd8330981d9dc92f81f2839
collegeGithubAccount <dvd8106@g.rit.edu>
2023-02-25 22:16:50 -0500

    added to weather and changed places to utilize csv

--- weather.py
+++ weather.py
@@ -1,7 +1,16 @@
 import math, csv
 
-def find_nearest():
-    with open('places.txt','r') as reader:
-        for line in reader:
-            line = line.split()
-            print(line)
+
+def find_nearest(inputCity):
+    with open('uscities.csv','r') as big:
+        with open('places.txt','r') as small:
+            a = [a.lower().strip().split(',')[1] for a in small]
+            b = [i[0:-2] for i in big if (i.lower().strip().split(',')[0] in a)]
+            
+            print(b)
+
+def main():
+    find_nearest('charlotte')
+
+
+main()
