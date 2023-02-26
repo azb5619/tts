9e7648e46a544cdd942d8e8bf32864d525099628
collegeGithubAccount <dvd8106@g.rit.edu>
2023-02-26 04:24:35 -0500

    added to weather and city

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 import math, csv, os
 
 
-def find_nearest(inputCity, inputState=None):
+def find_nearest(inputCity):
     big = open('uscities.csv','r')
     small = open('places.txt','r').readlines()
     a = [a.lower().strip().split(',')[1] for a in small]
@@ -19,14 +19,54 @@ def find_nearest(inputCity, inputState=None):
         if distance<shortest_lenght:
             shortest_lenght = distance
             closest = i
+    return closest
 
+
+def find_nearest_states(inputCity, inputState=None):
+    big = csv.reader(open('bigcitylist.csv'))
+    small = open('places.txt','r').readlines()
+    noaa_city_names = [[a.lower().strip().split(',')[1], a.split(',')[2][0:-1]] for a in small]
+
+    #noaacities = [[i, [b[0:-1] for b in small if i[3].lower().strip() in b.lower().strip()]] for i in big if ([i[3].lower().strip(), i[1]] in [[a[1].lower().strip(), a[2]] for a in small])]
+
+    #i[3].lower.strip() == big name
+    #i[1] == big state
+    #a[1].lower.strip == small name
+    #a[2] == small state
+    #i big state + location
+
+    noaacities = [[i, [noaa[0:-1] for noaa in small if [i[3].lower().strip(), i[1]] in [[a[1].lower().strip(), a[2]] for a in small]]] for i in big] #if ([i[3].lower().strip(), i[1]] in [[a[1].lower().strip(), a[2]] for a in small])]
+
+    print(noaacities)
+
+    big = csv.reader(open('bigcitylist.csv'))
+    icity = [i for i in big if i[3].lower().strip() == inputCity.strip().lower() and (bool(i[1].lower().strip() == inputState.lower().strip()) if inputState else True)][0]
+
+    icitylat = icity[5]
+    icitylong = icity[6]
+    shortest_lenght = 9999999
+    a,b = 0,0
+    closest = None
+    for i in noaacities:
+        tokens = i[0] + i[1]
+        distance = math.dist([float(tokens[5]),float(tokens[6])],[float(icitylat), float(icitylong)])
+        print(tokens[1], " ", icity[1])
+        if tokens[1] == icity[1] and distance<shortest_lenght:
+            shortest_lenght = distance
+            a,b=tokens[5],tokens[6]
+            closest = i
+    print(icity)
+    print(icitylat, icitylong)
+    print(shortest_lenght)
+    print(closest)
     return closest
 
 def getImage(closestCity):
     return "https://radar.weather.gov/ridge/standard/"+closestCity[1][0].split(',')[0]+"_loop.gif"
 
 def main():
-    os.system('firefox '+getImage(find_nearest(input('what city? '))))
+    a, b = input("city ").split()
+    os.system('firefox '+getImage(find_nearest_states(a,b)))
 
 
 main()
