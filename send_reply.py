5516061f252ce5a461dca66894c8632a52292b61
azb5619 <azb5619@rit.edu>
2023-02-26 05:14:43 -0500

    implemented search modes

--- send_reply.py
+++ send_reply.py
@@ -4,20 +4,40 @@ from search_engine import toText
 
 app = Flask(__name__)
 
-@app.route("/sms", methods=['GET', 'POST'])
-def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Start our TwiML response
+def search(body):
     body = request.values.get('Body', None)
+    results = toText(body)
+    get_results(body, results)
+    body = request.values.get('Body', None)
+    select_result(body, results)
+
+def get_results(body, results):
     resp = MessagingResponse()
-
-    response = toText(body)
-    string = ""
-    for i in range(len(response)):
-        string += f"{i}: {response[i][0]}\n"
+    string = "Results: \n"
+    for i in range(len(results)):
+        string += f"{i}: {results[i][0]}\n"
     resp.message(string)
-
     return str(resp)
+
+def select_result(body, results):
+    resp = MessagingResponse()
+    return str(results[int(body)][1])
+
+def chatbot():
+    pass
+def weather():
+    pass
+
+@app.route("/", methods=['GET', 'POST'])
+def query_reply():
+    body = request.values.get('Body', None)
+    match body:
+        case '!c':
+            pass
+        case '!w':
+            pass
+        case _:
+            search(body)
 
 if __name__ == "__main__":
     app.run(debug=True)
