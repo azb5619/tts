01d547122a097785dfb844026ff0c5de20d9b363
collegeGithubAccount <dvd8106@g.rit.edu>
2023-02-25 19:39:44 -0500

    files

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # tts
-Brickhack project to make a sms-based browser
+
+TTS, short for text to search is a comprehensive text messaging platform which allows you to use search the internet through sms.
 
 ## installation
 ```
-pip3 install -r requirements
+python -m pip install -r requirements
 ```
 
 ## environment variables
 
+Create a file called '.env' with 
  ```
 TWILIO_ACCOUNT_SID=<account sid>
 TWILIO_AUTH_TOKEN=<auth token>
---
+++ gpt.py
@@ -0,0 +1,19 @@
+import openai, os
+
+text_string = input("question ")
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+            engine='text-davinci-002',
+            prompt=prompt,
+            max_tokens=255,
+            n=1,
+            stop=None,
+            temperature=.4
+            )
+    return response.choices[0].text.strip()
+
+response = generate_response(text_string)
+print(response)
