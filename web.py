a89b09039e3b5f15fcbc680015a5f94270d1f3ca
raf1843 <raf1843@rit.edu>
2023-02-26 00:20:18 -0500

    Created WSGI python file  Changes to be committed: 	new file:   web.py

---
+++ web.py
@@ -0,0 +1,12 @@
+from cheroot import wsgi
+from send_reply import app 
+
+addr = '0.0.0.0', 5000
+serv = wsgi.WSGIPathInfoDispatcher({'/sms': app})
+server = wsgi.Server(addr, app)
+
+if __name__ == '__main__':
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        server.stop()
